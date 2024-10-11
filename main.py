67442880d2501b380e33b4cefe25d136830351b9
Welat-E <welaterenug@gmail.com>
2024-10-11 10:38:51 +0200

    Updated the classes

--- main.py
+++ main.py
@@ -1,83 +1,63 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the db instance from Flask-SQLAlchemy
-
 db = SQLAlchemy()
 
 
-class Users(db.Model):
-    """
-    Represents a User in the database.
-    """
+class User(db.Model):
+    __tablename__ = "users"
 
-    __tablename__ = "Users"
     user_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    role = db.Column(db.String(5))
-    email = db.Column(db.String)
-    password = db.Column(db.String)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
-    # Define a relationship between User and  Author and Publisher
-    publisher = db.relationship("Publisher", backref="users", lazy=True)
-    author = db.relationship("Author", backref="users", lazy=True)
-
-    def __repr__(self):
-        return f"<User {self.first_name}>"
+    # Relationships
+    publishers = db.relationship("Publisher", backref="user", lazy=True)
+    authors = db.relationship("Author", backref="user", lazy=True)
+    publications = db.relationship("PublicationDetails", backref="user", lazy=True)
 
 
 class Publisher(db.Model):
-    """
-    Represents a Publisher in the database.
-    """
+    __tablename__ = "publisher"
 
-    __tablename__ = "Publisher"
-    user_id = db.Column(db.Integer)
-    publisher_name = db.Column(dn.String)
-
-    # Relationship between Publisher and Publication_Details
-    publication_details = db.relationship(
-        "Publication_Details", backref="publisher", lazy=True
-    )
-
-    def __repr__(self):
-        return f"<Publisher {self.publisher_name}>"
-
-
-class Publication_Details(db.Model):
-    """
-    Represents Publication_Details in the database.
-    """
-
-    __tablename__ = "Publication_Details"
-    user_id = db.Column(db.Integer)
-    book_id = db.Column(db.Integer)
-    price = db.Column(db.Integer)
-    country = db.Column(db.String)
-    units = db.Column(db.Integer)
-    release_date = db.Column(db.String)
-    cover_image = db.Column(db.String)
-    pages = db.Column(db.Integer)
-    chapters = db.Column(db.Integer)
-    Link = db.Column(db.String)
-
-    #Relationship between Publication_Details and Author and Book
-    author = db.relationship("Author", backref="Publication_Details", lazy=True)
-    book = db.relationship("Book", backref="Publication_Details", lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    publisher_name = db.Column(db.String, nullable=False)
 
 
 class Author(db.Model):
-    """
-    Represents Authors in the database.
-    """
+    __tablename__ = "author"
 
-    __tablename__ = "Author"
-    user_id = db.Column(db.Integer)
-    book_id = db.Column(db.Integer)
-    image_cover = db.Column(db.String)
-    birth_date = db.Column(db.String)
-
-    #Relationship between Author and Book
-    book = db.relationship("Book", backref="Author", lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    name = db.Column(db.String, nullable=False)
+    author_image = db.Column(db.Text)
+    birth_date = db.Column(db.Date)
 
 
+class Book(db.Model):
+    __tablename__ = "book"
+
+    book_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    release_date = db.Column(db.String)
+    cover_image = db.Column(db.String)
+    chapters = db.Column(db.Integer)
+    pages = db.Column(db.Integer)
+
+    # Relationships
+    publication_details = db.relationship(
+        "PublicationDetails", backref="book", lazy=True
+    )
+
+
+class PublicationDetails(db.Model):
+    __tablename__ = "publication_details"
+
+    book_id = db.Column(db.Integer, db.ForeignKey("book.book_id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    price = db.Column(db.Numeric)
+    country = db.Column(db.String)
+    units = db.Column(db.Integer)
+    link = db.Column(db.Text)
+    language = db.Column(db.String)
